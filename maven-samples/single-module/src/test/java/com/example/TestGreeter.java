7bdc9fef4a35e0fae828b4593edb6efe0fd1443f
Vedant Yadav <05vedant@gmail.com>
2021-07-16 00:03:06 +0200

    pooling testing

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
   
   @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMessageTest() {
     String someone = "World, Thanks";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
